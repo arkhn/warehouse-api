99443abdddbbba569b53fb7a90e5d84e91357ade
elsiehoffet-94 <56258130+elsiehoffet-94@users.noreply.github.com>
2020-03-03 12:35:08 +0100

    addres comments

--- fhir-api/subsearch/search.py
+++ fhir-api/subsearch/search.py
@@ -45,13 +45,13 @@ def clean_params(parsed_params):
 def process_params(search_args):
 
     parsed_params = parse_params(search_args)
-
+    # TODO: handle offset
     offset = 0
     elements = None
 
     result_size = parsed_params.pop("_count", None)
-    total = int(result_size[0]) if result_size else 100
-    count = (
+    result_size = int(result_size[0]) if result_size else 100
+    is_summary_count = (
         True
         if parsed_params.get("_summary") and parsed_params["_summary"][0] == "count"
         else False
@@ -60,16 +60,16 @@ def process_params(search_args):
     if parsed_params.get("_summary") and parsed_params["_summary"][0] == "text":
         elements = ["text", "id", "meta"]
     elif parsed_params.get("_element"):
-        elements = parsed_params.pop("_element")
+        elements = parsed_params.get("_element")
     elif parsed_params.get("multiple"):
-        elements = parsed_params["multiple"].pop("_element", None)
+        elements = parsed_params["multiple"].get("_element", None)
 
     cleaned_params = clean_params(parsed_params)
 
-    return cleaned_params, total, elements, count, offset
+    return cleaned_params, result_size, elements, is_summary_count, offset
 
 
-def error_handler_count(Model, processed_params):
+def count_model(Model, processed_params):
     try:
         return Model(id).count(processed_params)
     except elasticsearch.exceptions.NotFoundError as e:
@@ -82,9 +82,9 @@ def error_handler_count(Model, processed_params):
         raise OperationOutcome(e)
 
 
-def error_handler_search(Model, processed_params, offset, total, elements):
+def search_model(Model, processed_params, offset, result_size, elements):
     try:
-        return Model(id).search(processed_params, offset, total, elements)
+        return Model(id).search(processed_params, offset, result_size, elements)
     except elasticsearch.exceptions.NotFoundError as e:
         raise OperationOutcome(
             f"{e.info['error']['index']} is not indexed in the database yet."
