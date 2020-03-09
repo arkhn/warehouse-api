0ff49ba94374d72cfb94d70e4d5e1d3dba8ed715
elsiehoffet-94 <56258130+elsiehoffet-94@users.noreply.github.com>
2020-03-09 14:31:09 +0100

    add new T2A route

--- fhir-api/api.py
+++ fhir-api/api.py
@@ -9,9 +9,15 @@ from subsearch.search import (
     resource_count,
     resource_search,
 )
+
 from flask_cors import CORS
+from urllib.parse import parse_qs
+from pandas import read_json, to_json, merge
 
 api = Blueprint("api", __name__)
+# enable Cross-Origin Resource Sharing
+# "Allow-Control-Allow-Origin" HTTP header
+CORS(api)
 
 
 @api.route("/<resource_type>/<id>", methods=["GET"])
@@ -94,17 +100,107 @@ def search(resource_type):
 
     search_args = {key: request.args.getlist(key) for key in request.args.keys()}
 
-    processed_params, result_size, elements, is_summary_count, offset = process_params(search_args)
+    processed_params, result_size, elements, is_summary_count, offset = process_params(
+        search_args
+    )
     Model = resources_models[resource_type]
 
     if is_summary_count:
         results = resource_count(Model, processed_params)
     else:
-        results = resource_search(Model, processed_params, offset, result_size, elements)
+        results = resource_search(
+            Model, processed_params, offset, result_size, elements
+        )
 
     if not results:
         raise OperationOutcome(f"No {resource_type} matching search criterias")
     return jsonify(results)
+
+
+def search_from_url_to_json(resource_type, url_args):
+    if resource_type not in resources_models:
+        raise OperationOutcome("Unknown resource type")
+
+    search_args = parse_qs(url_args)
+
+    processed_params, result_size, elements, is_summary_count, offset = process_params(
+        search_args
+    )
+    Model = resources_models[resource_type]
+
+    if is_summary_count:
+        results = resource_count(Model, processed_params)
+    else:
+        results = resource_search(
+            Model, processed_params, offset, result_size, elements
+        )
+
+    if not results:
+        raise OperationOutcome(f"No {resource_type} matching search criterias")
+    return read_json(jsonify(results), orient="records")
+
+
+@api.route("/t2a", methods=["GET"])
+def t2a(start_chemo, end_chemo):
+
+    products = search_from_url_to_json(
+        "MedicinalProductPharmaceutical",
+        "administrableDoseForm=gt0&_element=characteristics,ingredient,status,identifier,administrableDoseForm",
+    )
+    medicationrequest = search_from_url_to_json(
+        "MedicationRequest",
+        f"performedOn=gt{start_chemo}&performedOn=lt{end_chemo}&_element=detectedIssue,authoredOn,identifier,SupportingInformation,status,priorRequest,basedOn,performedOn",
+    )
+    medicationadministration = search_from_url_to_json(
+        "MedicationAdministration",
+        f"datetime=gt{start_chemo}&datetime=lt{end_chemo}&_element=dosage,datetime",
+    )
+    episodeofcare = search_from_url_to_json("EpisodeOfCare", "_element=period.start")
+    patient = search_from_url_to_json(
+        "Patient", "_element=identifier.value&subject.identifier.value"
+    )
+    organization = search_from_url_to_json("Organization", "_element=identifier,partOf")
+
+    ##group Patient, Episode of care and organization together
+    # key1 and 2 not named the same
+    group1 = merge(
+        medicationrequest,
+        organization,
+        left_on=["supportingInformation.identifier.value"],
+        right_on=["identifier.value"],
+        how="left",
+    )
+    group2 = merge(
+        group1,
+        products,
+        left_on=["medication.medicationReference.identifier.value"],
+        right_on=["ingredient.ingredient.identifier.value"],
+        how="left",
+    )
+    group3 = merge(
+        episodeofcare,
+        patient,
+        left_on=["subject.identifier.value"],
+        right_on=["identifier.value"],
+        how="left",
+    )
+    group4 = merge(
+        medicationadministration,
+        group3,
+        left_on=["subject.identifier.value"],
+        how="left",
+    )
+    final = merge(
+        group4,
+        group2,
+        left_on=["request.identifier.value"],
+        right_on=["identifier.value"],
+        how="outer",
+    )
+    
+    # final.groupby("")
+
+    return final.to_json
 
 
 @api.errorhandler(OperationOutcome)
