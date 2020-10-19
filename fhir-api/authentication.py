ff7484dab286fc04ece3b1304e3a64fac8cd4f6d
Jasopaum <jason.paumier@gmail.com>
2020-10-19 15:24:56 +0200

    Cache token introspection

--- fhir-api/authentication.py
+++ fhir-api/authentication.py
@@ -3,12 +3,16 @@ from functools import wraps
 import os
 import requests
 
+from cachetools import cached, TTLCache
+
 from errors import AuthenticationError
 
 TOKEN_INTROSPECTION_URL = os.getenv("TOKEN_INTROSPECTION_URL")
 AUTH_DISABLED = True if os.getenv("AUTH_DISABLED", "").lower() in ["1", "true", "yes"] else False
 
 
+# cache token introspection for 10 seconds
+@cached(cache=TTLCache(maxsize=128, ttl=10))
 def validate_token(token, scope=None):
     payload = {"token": token}
     if scope is not None:
