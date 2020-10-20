e83ea2d869a7702273f6063d999f33709c1c6400
Valentin Matton <taq@protonmail.ch>
2020-10-20 16:45:54 +0200

    [docker] Move FLASK_APP to entrypoint

--- fhir-api/docker-entrypoint.sh
+++ fhir-api/docker-entrypoint.sh
@@ -3,6 +3,8 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+export FLASK_APP="${FLASK_APP:-fhir_api/app}"
+
 # Load FHIR definitions
 flask load-defs /var/data/definitions
 
